18152dc6caca75ec28cdbf723e8c7f11c93f7553
Florian Mittlböck <florian.mittlboeck25@gmail.com>
2023-05-23 13:40:09 +0200

    Update MariaDBConnector.php

--- php/MariaDBConnector.php
+++ php/MariaDBConnector.php
@@ -16,15 +16,12 @@ $servername = 'localhost';
 $username = 'InfoDeepFake';
 $password = 'Linihlrf.KHOC9FV';
 $dbname = 'InfoDeepFake';
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
-
 // Retrieve data from the database
 $query = 'SELECT * FROM InfoBlocks';
 $result = $conn->query($query);
@@ -36,11 +33,12 @@ if ($result->num_rows > 0) {
         $data[] = utf8ize($row);
     }
 }
-
 // Close the database connection
 $conn->close();
-
 // Send the JSON response
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 echo json_encode($data);
 
