67217961b90fe1856ed382249b32256a44a3964f
maires47 <s2210892013@fhooe.at>
2023-06-01 13:00:08 +0200

    update

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@
       
   });
 
- //JSON-Daten abrufen und anzeigen
-fetch('https://infodeepfake.projekte.fh-hagenberg.at/php/MariaDBConnector.php')
+ //JSON-Daten abrufen und anzeigen Deutsch
+fetch('https://infodeepfake.projekte.fh-hagenberg.at/php/MariaDBConnector.php?lang=de')
   .then(response => response.json())
   .then(data => displayJSONData(data))
   .catch(error => console.log(error));
@@ -38,33 +38,79 @@ function displayJSONData(data) {
       //HTML-Elemente mit den Daten aus der JSON-Datei füllen
       titleElement.textContent = item.Title;
       previewTextElement.textContent = item.IntroText;
-      //fullTextElement.textContent = item.fullText;
-
-      //Überprüfen, welche Sprache ausgewählt ist und entsprechend die Daten setzen
-      //const selectedLanguage = item.sprache;
-      // if (selectedLanguage === 'EN') {
-      //  titleElement.textContent = item.TitleEN;
-      //  previewTextElement.textContent = item.IntroTextEN;
-      //  fullTextElement.textContent = item.MainTextEN;
-
-      //} else {
-      //  titleElement.textContent = item.Title;
-      //  previewTextElement.textContent = item.IntroText;
-      //  fullTextElement.textContent = item.MainText;
-      //}
-
-      // Klickereignis für die Box hinzufügen
-      //box.addEventListener('click', toggleFullText);
     }
   }
 }
 
-//function toggleFullText(event) {
-//  const box = event.currentTarget;
-//  const fullTextElement = box.querySelector('.full-text'); -> full-text div definieren??
-//
-//  fullTextElement.style.display = fullTextElement.style.display === 'none' ? 'block' : 'none';
-//}
+fetch('https://infodeepfake.projekte.fh-hagenberg.at/php/MariaDBConnector.php?lang=de')
+  .then(response => response.json())
+  .then(data => displayFullText(data))
+  .catch(error => console.log(error));
+
+function displayFullText(data) {
+  const sections = document.getElementsByClassName('sectionfulltext');
+
+  // Überprüfen, ob die JSON-Daten vorhanden sind und die Anzahl der Sections korrekt ist
+  if (data && data.length >= sections.length) {
+
+    // Schleife über die Sections und die JSON-Daten
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const item = data[i];
+
+      // HTML-Element mit der Klasse "fullText" in der aktuellen Section finden
+      const fullTextElement = section.querySelector('.fullText');
+
+      // MainText aus den Daten in das HTML-Element einfügen
+      fullTextElement.textContent = item.MainText;
+    }
+  }
+}
+
+// Funktion, um auf die entsprechende Section zu springen
+function goToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
+
+// Event-Handler für die Boxen
+document.addEventListener('DOMContentLoaded', function() {
+  const box1 = document.getElementById('box1');
+  const box2 = document.getElementById('box2');
+  const box3 = document.getElementById('box3');
+  const box4 = document.getElementById('box4');
+  const box5 = document.getElementById('box5');
+  const box6 = document.getElementById('box6');
+
+  box1.addEventListener('click', function() {
+    goToSection('intro');
+  });
+
+  box2.addEventListener('click', function() {
+    goToSection('einsatz');
+  });
+
+  box3.addEventListener('click', function() {
+    goToSection('quellen');
+  });
+
+  box4.addEventListener('click', function() {
+    goToSection('tipps');
+  });
+
+  box5.addEventListener('click', function() {
+    goToSection('regierung');
+  });
+
+  box6.addEventListener('click', function() {
+    goToSection('news');
+  });
+});
+
+
+
 
 
 
